826f4eb9e1e9198357106f3e2127a6dcc45a0909
Oluwatosin Ikudayisi <oluwatosin.ikudayisi@cashdrive.co>
2020-07-07 15:34:36 +0100

    get country by name

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// 1
+app.post('/countries', async (req,res) => {
+	await fetch('https://restcountries.eu/rest/v2/name/'+req.body.name)
+		.then(res => res.json())
+		.then(data => res.send(200, {data : data, status : 'success'}))
+});
 
 
 app.listen(process.env.PORT);
